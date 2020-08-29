4d968b013463fe5c016052fa37e449d3bbc86e11
Davide Rubini <drubini96@gmail.com>
2020-08-29 18:41:46 +0200

    some changes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var tl = new TimelineLite({delay: '0.9', clearProps: 'all'})
+var tl = new TimelineLite({delay: '0.3', clearProps: 'all'})
     .from('#section1 i', 0.9, {autoAlpha: 0, y: 15, ease: Power2.easeOut}, '0.06','+=0')
     .staggerFrom('#section1 h1', 0.9, {autoAlpha: 0, y: 35, ease: Power2.easeOut},'0.06', '-=0.6')
     .from('#section1 p', 0.9, {autoAlpha: 0, y: 15, ease: Power2.easeOut}, '0.08','-=0.8')
